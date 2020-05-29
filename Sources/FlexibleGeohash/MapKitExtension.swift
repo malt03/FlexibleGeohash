e7b21a2334bbff357ffd060215d7b64102952b26
Koji Murata <koji.murata@dena.com>
2020-05-29 20:30:08 +0900

    mapkit

---
+++ Sources/FlexibleGeohash/MapKitExtension.swift
@@ -0,0 +1,19 @@
+//
+//  MapKitExtension.swift
+//  FlexibleGeohash
+//
+//  Created by Koji Murata on 2020/05/29.
+//
+
+#if canImport(MapKit)
+import MapKit
+
+extension CLLocationCoordinate2D: LatLngProtocol {}
+extension LatLng {
+    func clCoordinate() -> CLLocationCoordinate2D { .init(latitude: latitude, longitude: longitude) }
+    func mkSpan() -> MKCoordinateSpan { .init(latitudeDelta: latitude, longitudeDelta: longitude) }
+}
+extension Region {
+    func mk() -> MKCoordinateRegion { .init(center: center.clCoordinate(), span: span.mkSpan()) }
+}
+#endif
